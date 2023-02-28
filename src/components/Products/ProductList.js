7e39d026facea4aef2f15968dd797915c71edc55
atlefar <atle.carlsen@live.no>
2023-02-28 13:32:02 +0100

    Started on check of function

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -3,23 +3,22 @@ import Card from '../UI/Card';
 import classes from './ProductList.module.css';
 
 const ProductList = (props) => {
-  // const clickHandler = (event) => {
-  //   if (event.target.style.textDecoration) {
-  //     event.target.style.removeProperty('text-decoration');
-  //   } else {
-  //     event.target.style.setProperty('text-decoration', 'line-through');
-  //   }
-  // };
+  const checkProductHandler = (event) => {
+    if (event.target.style.textDecoration) {
+      event.target.style.removeProperty('text-decoration');
+    } else {
+      event.target.style.setProperty('text-decoration', 'line-through');
+    }
+  };
   
     return (
       <Card className={classes.product}> 
         <ul>
             {props.product.map((product) => (
                 <li key={product.id} 
-                // onClick={clickHandler}
                 >
-                    {product.product}
-                    <Button onClick={() => props.onRemove(product.id)}>Slett</Button>
+                  <span onClick={checkProductHandler}>{product.product}</span>
+                  <Button onClick={() => props.onRemove(product.id)}>Slett</Button>
                 </li>
                 ))}
         </ul>
